3409dcfa05acb67314a83d55190e73780dfc7c32
Yash Pandey <yashpandey1556@gmail.com>
2025-07-12 20:22:15 +0530

    booo

---
+++ A_Hexadecimal_s_theorem.cpp
@@ -0,0 +1,16 @@
+    #include <iostream>
+    #include <vector>
+    #include <algorithm>
+    using namespace std;
+
+    int main(){
+        int n;
+        cin >> n;
+
+        cout << "0" << " "<< "0" << " " << n;
+        
+
+        
+        
+        return 0;
+    }
